c56a27533a003b3b49372fc19befca307ac48e97
Aman Gondaliya <35319382+Amangondaliya555@users.noreply.github.com>
2020-10-07 17:16:02 +0530

    Update rsa.py

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,3 +1,19 @@
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(coprime, phi_n):
+    g, x, y = egcd(coprime, phi_n)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % phi_n
+
+
 encrypt():
 
 decrypt():
